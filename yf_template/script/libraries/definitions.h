095f9798ba427265ae7644a8bd5408f9f9756568
francesco <fg.minisini@gmail.com>
2024-05-22 20:02:46 +0200

    Your commit message

---
+++ yf_template/script/libraries/definitions.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+struct stock
+{
+    string date;
+    double open;
+    double high;
+    double low;
+    double close;
+    double volume;
+    double dividends;
+    double stock_splits;
+};
+
+stock* initialize_stock_values(string directory, int& n_stock, int& error_code);
+void print_data (stock* , int , int& ); 
